f6a6b6f443e165634a7fd4825d2e805fada6e4dc
Erikacreatto <erikacreatto@gmail.com>
2026-03-30 09:45:54 -0300

    Teste: erro de sintaxe

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def home(:
     return "Aula 3 – Arquitetura da Integração Contínua e GitHub Actions 🚀"
 
 if __name__ == "__main__":

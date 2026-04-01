32dbd668fc5b0f9363eb928567861bbb1a57dfa4
Erikacreatto <erikacreatto@gmail.com>
2026-04-01 20:59:44 -0300

    fix: corrige teste da rota home

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,9 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
+
+    texto = response.data.decode('utf-8')
+
     assert response.status_code == 200
-    assert b"Aula 4" in response.data
+    assert "Aula 4" in texto
 

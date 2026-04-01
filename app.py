1bf4308eca8097c46423cf3e1ffacae534d53689
Erikacreatto <erikacreatto@gmail.com>
2026-04-01 19:52:00 -0300

    test: simula falha na rota home

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Aula 3 – Arquitetura da Integração Contínua e GitHub Actions 🚀"
+    return "Erro na aplicação"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
